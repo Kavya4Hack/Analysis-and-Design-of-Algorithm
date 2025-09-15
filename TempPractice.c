bb1c7e2ef65f6b46fb926f79686a71ce85a6f4bf
Kavya4Hack <kavya.nayi08@gmail.com>
2025-09-16 01:41:26 +0530

    TempPractice changed

--- TempPractice.c
+++ TempPractice.c
@@ -887,31 +887,31 @@
 //     return 0;
 // }
 // insertion sort
-#include <stdio.h>
-void insertion(int arr[], int n){
-    for(int i=1;i<n;i++){
-        int key = arr[i];
-        int j = i - 1;
-        while(j>=0 && arr[j]>key){
-            arr[j+1] = arr[j];
-            j = j - 1;
-        }
-        arr[j+1] = key;
-    }
-}
-void printArray(int arr[], int n){
-    for(int i=0;i<n;i++){
-        printf("%d ",arr[i]);
-    }
-    printf("\n");
-}
-int main(){
-    int arr[] = {15,21,47,55,1,20,33,5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Unsorted array: ");
-    printArray(arr,n);
-    printf("Sorted array: ");
-    insertion(arr,n);
-    printArray(arr,n);
-    return 0;
-}
+// #include <stdio.h>
+// void insertion(int arr[], int n){
+//     for(int i=1;i<n;i++){
+//         int key = arr[i];
+//         int j = i - 1;
+//         while(j>=0 && arr[j]>key){
+//             arr[j+1] = arr[j];
+//             j = j - 1;
+//         }
+//         arr[j+1] = key;
+//     }
+// }
+// void printArray(int arr[], int n){
+//     for(int i=0;i<n;i++){
+//         printf("%d ",arr[i]);
+//     }
+//     printf("\n");
+// }
+// int main(){
+//     int arr[] = {15,21,47,55,1,20,33,5};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     printf("Unsorted array: ");
+//     printArray(arr,n);
+//     printf("Sorted array: ");
+//     insertion(arr,n);
+//     printArray(arr,n);
+//     return 0;
+// }
