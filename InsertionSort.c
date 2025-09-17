b7f5cdc6652457b7ea9b2243d2b11677c3ad270d
Kavya4Hack <kavya.nayi08@gmail.com>
2025-09-17 17:35:28 +0530

    main fun. changes

--- InsertionSort.c
+++ InsertionSort.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void insertionSort(int arr[], int n){           // [5,8,7,1,4,3]
+void insertionSort(int arr[], int n){           
     for(int i=1;i<n;++i){                       
-        int key = arr[i];                       // key = 8
-        int j = i - 1;                          // j = 0
-        while(j>=0 && arr[j]>key){              // case 1: 0>=0 && 5>8 --> False. So, key = 8
-            arr[j+1] = arr[j];                  // case 2:  key = 7 j=1 ; 1>=0 && 8>7 --> arr[2]=8 j=0
+        int key = arr[i];                       
+        int j = i - 1;                          
+        while(j>=0 && arr[j]>key){              
+            arr[j+1] = arr[j];                  
             j = j-1;
         }
         arr[j+1] = key;
