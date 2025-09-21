533aea69fc34df39936cf50142b06cbc37fc03a6
Kavya4Hack <kavya.nayi08@gmail.com>
2025-09-21 18:27:37 +0530

    Kruskal added

--- TempPractice.c
+++ TempPractice.c
@@ -2112,73 +2112,160 @@ int main(){
     printf("minCost = %d\n", minCost);
     return 0;
 }
+// #include <stdio.h>
+
+// #define MAX 100
+// #define INF 999
+// int graph[MAX][MAX];
+// int visited[MAX];
+
+// int prims(int vertices, int start){
+
+//     int minCost = 0;
+//     int minEdge_Cost = INF;
+//     int u , v;
+//     visited[start] = 1;
+
+//     for (int count = 0; count < vertices - 1; count++){
+//         minEdge_Cost = INF;
+        
+//         for (int i = 0; i < vertices; i++){
+//             if (visited[i]) {
+//                 for (int j = 0; j < vertices; j++){
+//                     if (graph[i][j] && !visited[j] && graph[i][j] < minEdge_Cost) {
+//                         minEdge_Cost = graph[i][j];
+//                         u = i;
+//                         v = j;
+//                     }
+//                 }
+                
+//             }
+//         }
+//         if (minEdge_Cost == INF) {
+//             printf("No MST Possible!!\n");
+//             return -1;
+//         }
+//         else {
+//             visited[v] = 1;
+//             minCost += minEdge_Cost;
+//             printf("(%d -> %d) => %d\n" , u , v, minEdge_Cost);
+//         }
+//     }
+//     return minCost;  
+// }
+
+// int main(){
+//     int vertices, edges, u, v, weight, start = 0;
+
+//     printf("Enter the number of vertices: ");
+//     scanf("%d", &vertices);
+
+//     printf("Enter the number of edges: ");
+//     scanf("%d", &edges);
+
+//     for (size_t i = 0; i < vertices; i++){
+//         visited[i] = 0;
+//     }
+//     for (int i = 0; i < vertices; i++){
+//         for (int j = 0; j < vertices; j++){
+//             graph[i][j] = 0;
+//         }
+//     }
+//     printf("Enter the Edges: (u,v,weight) \n");
+//     for (int i = 0; i < edges; i++){
+//         scanf("%d %d %d", &u, &v, &weight);
+
+//         graph[u][v] = weight;
+//         graph[v][u] = weight;
+//     }
+//     int minCost = prims(vertices, start);
+//     printf("minCost = %d\n", minCost);
+//     return 0;
+// }
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX 100
-#define INF 999
-int graph[MAX][MAX];
-int visited[MAX];
 
-int prims(int vertices, int start){
+int rank[MAX];
+int parent[MAX];
 
-    int minCost = 0;
-    int minEdge_Cost = INF;
-    int u , v;
-    visited[start] = 1;
+typedef struct {
+    int src , dest , weight;
+} Edge;
 
-    for (int count = 0; count < vertices - 1; count++){
-        minEdge_Cost = INF;
-        
-        for (int i = 0; i < vertices; i++){
-            if (visited[i]) {
-                for (int j = 0; j < vertices; j++){
-                    if (graph[i][j] && !visited[j] && graph[i][j] < minEdge_Cost) {
-                        minEdge_Cost = graph[i][j];
-                        u = i;
-                        v = j;
-                    }
-                }
-                
-            }
-        }
-        if (minEdge_Cost == INF) {
-            printf("No MST Possible!!\n");
-            return -1;
-        }
-        else {
-            visited[v] = 1;
-            minCost += minEdge_Cost;
-            printf("(%d -> %d) => %d\n" , u , v, minEdge_Cost);
-        }
-    }
-    return minCost;  
+Edge edgeList[MAX];
+
+int find(int v) {
+    if (parent[v] != v)
+        parent[v] = find(parent[v]);
+    return parent[v];
 }
 
-int main(){
-    int vertices, edges, u, v, weight, start = 0;
+int unionSets(int u , int v) {
+    int rootU = find(u);
+    int rootV = find(v);
 
-    printf("Enter the number of vertices: ");
-    scanf("%d", &vertices);
+    if (rootU != rootV) {
 
-    printf("Enter the number of edges: ");
-    scanf("%d", &edges);
-
-    for (size_t i = 0; i < vertices; i++){
-        visited[i] = 0;
-    }
-    for (int i = 0; i < vertices; i++){
-        for (int j = 0; j < vertices; j++){
-            graph[i][j] = 0;
+        if (rank[rootU] > rank[rootV]) {
+            parent[rootV] = rootU;
+        }
+        else if (rank[rootV] > rank[rootU]) {
+            parent[rootU] = rootV;
+        }
+        else {
+            parent[rootV] = rootU;
+            rank[rootU]++;
         }
     }
-    printf("Enter the Edges: (u,v,weight) \n");
+}
+
+int cmp(const void *a , const void *b) {
+    Edge *e1 = (Edge *)a;
+    Edge *e2 = (Edge *)b;
+    return e1 -> weight - e2 -> weight;
+}
+int kruskal(int vertices , int edges) {
+    for (int i = 0; i < vertices; i++){
+        parent[i] = i;
+        rank[i] = 0;
+    }
+    int minCost = 0 , edgeCount = 0;
+    qsort(edgeList , edges , sizeof(Edge) , cmp);
+
+    for (int i = 0; i < edges && edgeCount < vertices; i++){
+        int src = edgeList[i].src;
+        int dest = edgeList[i].dest;
+        int weight = edgeList[i].weight;
+
+        if (find(src) != find(dest)) {
+
+            unionSets(src ,dest);
+            minCost += weight;
+            edgeCount++;
+            printf("(%d -> %d) => %d\n" , src ,dest , weight);
+        }
+    }
+    return minCost;
+    
+}
+
+int main() {
+    int vertices, edges, u, v, weight;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &vertices);
+    printf("Enter the number of edges: ");
+    scanf("%d", &edges);
+    printf("Enter the edges: ");
     for (int i = 0; i < edges; i++){
         scanf("%d %d %d", &u, &v, &weight);
-
-        graph[u][v] = weight;
-        graph[v][u] = weight;
+        edgeList[i].src = u;
+        edgeList[i].dest = v;
+        edgeList[i].weight = weight;
     }
-    int minCost = prims(vertices, start);
-    printf("minCost = %d\n", minCost);
+    int minCost = kruskal(vertices, edges);
+    printf("MinCost = %d ", minCost);
+
     return 0;
 }
